799b8563ce5b85567970355ee00b0f68582054f1
Juan Pablo Perez <Juan.Pablo.Perez@retailinmotion.com>
2018-09-16 12:24:28 +0100

    8.-Creating a Responsive User Interface (UI)

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -4,7 +4,8 @@ var closeCreatePostModalButton = document.querySelector('#close-create-post-moda
 var sharedMomentsArea = document.querySelector('#shared-moments');
 
 function openCreatePostModal() {
-  createPostArea.style.display = 'block';
+  // createPostArea.style.display = 'block';
+  createPostArea.style.transform = 'translateY(0)';
   if (deferredPrompt) {
     deferredPrompt.prompt();
 
@@ -33,7 +34,8 @@ function openCreatePostModal() {
 }
 
 function closeCreatePostModal() {
-  createPostArea.style.display = 'none';
+  createPostArea.style.transform = 'translateY(100vh)';
+  // createPostArea.style.display = 'none';
 }
 
 shareImageButton.addEventListener('click', openCreatePostModal);
